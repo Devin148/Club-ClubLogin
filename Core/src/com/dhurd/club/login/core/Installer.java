0d2b2a95be7fd08a058e8e4041dff11c31e5d400
Devin148 <devinhurd@gmail.com>
2013-02-21 14:23:36 -0500

    Change application size

--- Core/src/com/dhurd/club/login/core/Installer.java
+++ Core/src/com/dhurd/club/login/core/Installer.java
@@ -1,20 +1,35 @@
 package com.dhurd.club.login.core;
 
 import com.dhurd.club.login.coreutils.CorePaths;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.text.MessageFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
 import org.openide.DialogDisplayer;
 import org.openide.LifecycleManager;
 import org.openide.WizardDescriptor;
 import org.openide.modules.ModuleInstall;
+import org.openide.windows.WindowManager;
 
 public class Installer extends ModuleInstall {
     private static final Logger logger = Logger.getLogger(Installer.class.getName());
 
     @Override
     public void restored() {
+        // Set the size of the application
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Frame f = WindowManager.getDefault().getMainWindow();
+                f.addWindowListener(new Listener());
+            }
+        });
+        
+        // Check if the database exists
         String dbPath = CorePaths.DATABASE_PATH;
         File db = new File(dbPath);
         if (!db.exists()) {
@@ -33,5 +48,14 @@ public class Installer extends ModuleInstall {
              }
         }
         
+    }
+    
+    // Listener for setting size, because the size is reset after installation:
+    private class Listener extends WindowAdapter {
+        @Override
+        public void windowActivated(WindowEvent event) {
+            Frame f = WindowManager.getDefault().getMainWindow();
+            f.setSize(1280, 720);
+        }
     }
 }
